cda28690d10d4dae2c9589734b817984dfa6e3f8
Jerry Chen <jerryc443@gmail.com>
2024-01-31 23:06:28 -0500

    use server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,6 @@
 import { argv } from 'process'
 import { parseSTMTTRN } from '@/utils'
+import {encode} from 'cbor-x'
 
 const filename = argv[2]
 const file = Bun.file(filename)
@@ -8,7 +9,16 @@ const content = await file.text()
 const STMTTRN_RE = /<STMTTRN>.+?<\/STMTTRN>/gs
 const stmtTrnMatches = content.match(STMTTRN_RE)
 
-if (stmtTrnMatches) {
-  const trns = stmtTrnMatches.map(stmtTrn => parseSTMTTRN(stmtTrn, -1))
-  console.dir(trns)
+if (!stmtTrnMatches) {
+  throw new Error('No STMTTRN matched!')
 }
+
+const trns = stmtTrnMatches.map(stmtTrn => parseSTMTTRN(stmtTrn, -1))
+
+const server = Bun.serve({
+  fetch(request, server) {
+    return new Response(encode(trns).toString('base64'))
+  },
+})
+
+console.log(`Server running at ${server.url}`)
